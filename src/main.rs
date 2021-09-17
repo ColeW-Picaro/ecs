cbe78d265ea9f58fd871b19e2a7e014fd90980d6
Cole Vohs <colevohs@gmail.com>
2021-09-16 23:06:44 -0400

    add example

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
-use std::{any::Any, borrow::BorrowMut, cell::{RefCell, RefMut}};
+use std::{
+    any::Any,
+    borrow::BorrowMut,
+    cell::{RefCell, RefMut},
+};
+
+use components::*;
 
 pub mod components;
 
-fn main() {}
+fn main() {
+    let mut entity_manager = EntityManager {
+        entity_count: 0,
+        components: Vec::new(),
+    };
+
+    let place_ent = entity_manager.add_entity();
+    entity_manager.add_component_to_entity(place_ent, Position { x: 1, y: 1 });
+
+    let moving_ent = entity_manager.add_entity();
+    entity_manager.add_component_to_entity(moving_ent, Velocity { vel: 1.0 });
+
+    let place_and_moving_ent = entity_manager.add_entity();
+    entity_manager.add_component_to_entity(place_and_moving_ent, Position { x: 1, y: 1 });
+    entity_manager.add_component_to_entity(place_and_moving_ent, Velocity { vel: 1.0 });
+
+    let pos_store= entity_manager
+        .borrow_component_store::<Position>()
+        .unwrap();
+    let vel_store = entity_manager
+        .borrow_component_store::<Velocity>()
+        .unwrap();
+    let zip = pos_store.iter().zip(vel_store.iter());
+    for (pos, vel) in zip.filter_map(|(pos, vel)| Some((pos.as_ref()?, vel.as_ref()?))) {
+        println!("Position: x: {}, y: {}", pos.x, pos.y);
+        println!("Velocity: {}", vel.vel);
+    }
+}
 
 pub struct EntityManager {
     entity_count: usize,
     components: Vec<Box<dyn Storage>>,
-
 }
 
+#[derive(Debug)]
 pub struct ComponentError {
-    msg: String
+    msg: String,
 }
 
 impl EntityManager {
-
-
     fn add_entity(&mut self) -> usize {
         let entity_id = self.entity_count;
         for component in self.components.iter_mut() {
@@ -28,17 +59,24 @@ impl EntityManager {
 
     fn borrow_component_store<T: 'static>(&self) -> Result<RefMut<Vec<Option<T>>>, ComponentError> {
         for component_vec in self.components.iter() {
-            if let Some(component_vec) = component_vec.as_any().downcast_ref::<RefCell<Vec<Option<T>>>>()
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<RefCell<Vec<Option<T>>>>()
             {
                 return Ok(component_vec.borrow_mut());
             }
         }
-        Err(ComponentError { msg: String::from("Component does not exist")})
+        Err(ComponentError {
+            msg: String::from("Component does not exist"),
+        })
     }
 
     pub fn add_component_to_entity<T: 'static>(&mut self, entity: usize, component: T) {
         for component_store in self.components.iter_mut() {
-            if let Some(component_store) = component_store.as_any_mut().downcast_mut::<RefCell<Vec<Option<T>>>>() {
+            if let Some(component_store) = component_store
+                .as_any_mut()
+                .downcast_mut::<RefCell<Vec<Option<T>>>>()
+            {
                 component_store.get_mut()[entity] = Some(component);
                 return;
             }
@@ -49,7 +87,8 @@ impl EntityManager {
             new_component_store.push(None);
         }
         new_component_store[entity] = Some(component);
-        self.components.push(Box::new(RefCell::new(new_component_store)));
+        self.components
+            .push(Box::new(RefCell::new(new_component_store)));
     }
 }
 
@@ -70,7 +109,6 @@ impl<T: 'static> Storage for RefCell<Vec<Option<T>>> {
         self as &mut dyn Any
     }
 }
-
 
 mod tests {
     use super::*;
@@ -104,5 +142,34 @@ mod tests {
         let ent = entity_manager.add_entity();
 
         entity_manager.add_component_to_entity(ent, Position { x: 1, y: 2 });
+    }
+
+    #[test]
+    fn iterate_over_components() {
+        let mut entity_manager = EntityManager {
+            entity_count: 0,
+            components: Vec::new(),
+        };
+
+        let place_ent = entity_manager.add_entity();
+        entity_manager.add_component_to_entity(place_ent, Position { x: 1, y: 1 });
+
+        let moving_ent = entity_manager.add_entity();
+        entity_manager.add_component_to_entity(moving_ent, Velocity { vel: 1.0 });
+
+        let zip = entity_manager
+            .borrow_component_store::<Position>()
+            .unwrap()
+            .iter()
+            .zip(
+                entity_manager
+                    .borrow_component_store::<Velocity>()
+                    .unwrap()
+                    .iter(),
+            );
+        for (pos, vel) in zip.filter_map(|(pos, vel)| Some((pos.as_ref()?, vel.as_ref()?))) {
+            println!("Position: x: {}, y: {}", pos.x, pos.y);
+            println!("Velocity: {}", vel.vel);
+        }
     }
 }
