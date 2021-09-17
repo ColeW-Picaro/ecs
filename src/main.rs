9933ed16606a2bb98aa9eb0bf36686cccdb4ec8c
Cole Vohs <colevohs@gmail.com>
2021-09-16 22:33:01 -0400

    it compiled

--- src/main.rs
+++ src/main.rs
@@ -26,18 +26,6 @@ impl EntityManager {
         entity_id
     }
 
-    fn create_component_store<T: 'static>(&mut self) -> RefCell<Vec<Option<T>>> {
-        let mut new_component_store = Vec::<Option<T>>::with_capacity(self.entity_count);
-        for _ in 0..self.entity_count {
-            new_component_store.push(None);
-        }
-        RefCell::new(new_component_store)
-    }
-
-    fn register_component_store<T: 'static>(&mut self, component_store: RefCell<Vec<Option<T>>>) {
-        self.components.push(Box::new(component_store));
-    }
-
     fn borrow_component_store<T: 'static>(&self) -> Result<RefMut<Vec<Option<T>>>, ComponentError> {
         for component_vec in self.components.iter() {
             if let Some(component_vec) = component_vec.as_any().downcast_ref::<RefCell<Vec<Option<T>>>>()
@@ -48,37 +36,20 @@ impl EntityManager {
         Err(ComponentError { msg: String::from("Component does not exist")})
     }
 
-    pub fn register_component<T: 'static>(&mut self) {
-        let new_component_store = self.create_component_store::<T>();
-        self.register_component_store(new_component_store);
-    }
-
     pub fn add_component_to_entity<T: 'static>(&mut self, entity: usize, component: T) {
-        let component_store_result = self.borrow_component_store::<T>();
-        match component_store_result {
-            Ok(store) => store.borrow_mut()[entity] = Some(component),
-            Err(_) => self.register_component::<T>()
-        }
-    }
-
-    pub fn get_component_from_entity<T: 'static>(&mut self, entity: usize) -> Result<&T, ComponentError> {
-        let component_store_result = self.borrow_component_store::<T>();
-        match component_store_result {
-            Ok(store) => {
-                return match &store.get(entity) {
-                    Some(c) => {
-                        match c {
-                            Some()
-                        }
-                    },
-                    None => Err(ComponentError { msg: String::from("Entity does not have component") })
-                }
-            },
-            Err(_) => {
-                self.register_component::<T>();
-                Err(ComponentError { msg: String::from("Entity does not have component") })
+        for component_store in self.components.iter_mut() {
+            if let Some(component_store) = component_store.as_any_mut().downcast_mut::<RefCell<Vec<Option<T>>>>() {
+                component_store.get_mut()[entity] = Some(component);
+                return;
             }
         }
+
+        let mut new_component_store = Vec::<Option<T>>::with_capacity(self.entity_count);
+        for _ in 0..self.entity_count {
+            new_component_store.push(None);
+        }
+        new_component_store[entity] = Some(component);
+        self.components.push(Box::new(RefCell::new(new_component_store)));
     }
 }
 
@@ -90,7 +61,7 @@ trait Storage {
 
 impl<T: 'static> Storage for RefCell<Vec<Option<T>>> {
     fn allocate(&mut self) {
-        self.borrow().push(None);
+        self.get_mut().push(None);
     }
     fn as_any(&self) -> &dyn Any {
         self as &dyn Any
@@ -99,6 +70,7 @@ impl<T: 'static> Storage for RefCell<Vec<Option<T>>> {
         self as &mut dyn Any
     }
 }
+
 
 mod tests {
     use super::*;
@@ -123,57 +95,14 @@ mod tests {
     }
 
     #[test]
-    fn register_component() {
-        let mut entity_manager = EntityManager {
-            entity_count: 0,
-            components: Vec::new(),
-        };
-
-        entity_manager.register_component::<Position>();
-    }
-
-    #[test]
-    fn register_different_components() {
-        let mut entity_manager = EntityManager {
-            entity_count: 0,
-            components: Vec::new(),
-        };
-
-        entity_manager.register_component::<Position>();
-        entity_manager.register_component::<Velocity>();
-    }
-
-    #[test]
     fn add_component_to_entity() {
         let mut entity_manager = EntityManager {
             entity_count: 0,
             components: Vec::new(),
         };
 
-        entity_manager.register_component::<Position>();
-
         let ent = entity_manager.add_entity();
 
         entity_manager.add_component_to_entity(ent, Position { x: 1, y: 2 });
-    }
-
-    #[test]
-    fn get_component_from_entity() {
-        let mut entity_manager = EntityManager {
-            entity_count: 0,
-            components: Vec::new(),
-        };
-
-        entity_manager.register_component::<Position>();
-
-        let ent = entity_manager.add_entity();
-
-        entity_manager.add_component_to_entity(ent, Position { x: 1, y: 2 });
-
-        let pos = entity_manager.get_component_from_entity::<Position>(ent);
-        match pos {
-            Ok(p) => assert_eq!(1, p.x),
-            Err(e) => assert_eq!("component error", e.msg)
-        }
     }
 }
